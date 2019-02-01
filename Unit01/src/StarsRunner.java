d169eab6b6214d8a54c8442ccf23cb39e52acabb
CarolineBao <caroline.yb@gmail.com>
2019-02-01 12:36:06 -0800

    Added Unit01 files

---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,25 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes America = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      
+      America.printASmallBox();
+      
+      America.printTwoBlankLines();
+      
+      America.printABigBox();
+   }
+}
---
+++ Unit02/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author carolinebao
+ *
+ */
+module Unit02 {
+}
