353d764d1dc2a01a9a2acb387f61681ae0517a4b
CarolineBao <caroline.yb@gmail.com>
2019-03-11 13:06:00 -0700

    Updated Word

--- Unit12/src/WordRunner.java
+++ Unit12/src/WordRunner.java
@@ -12,21 +12,25 @@ public class WordRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("words.dat"));
+		Scanner file = new Scanner(new File("//Users//carolinebao//Documents//GitHub//bao_caroline_apcsa-p3//Unit12//WordsData//words.dat"));
 
 		int size = file.nextInt();
+		Word words[]=new Word[size];
+		int counter=0;
 		file.nextLine();
+		for (int i=0; i<size; i++)
+		{
+			words[counter]=new Word(file.nextLine());
+			counter++;
+		}
+		file.close();
 		
-
-
-
-
-
-
-
-
-
-
+		Arrays.sort(words);
+		
+		for (Word word:words)
+		{
+			System.out.println(word);
+		}
 
 	}
 }
